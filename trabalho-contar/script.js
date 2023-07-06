fe2e702056ee9079601c099e5aaa5554d683dc1e
layza-0 <132698296+layza-0@users.noreply.github.com>
2023-07-06 15:14:23 +0000

    08

--- trabalho-contar/script.js
+++ trabalho-contar/script.js
@@ -1,31 +1,32 @@
-function contar() {
+function contar(){
     let ini = document.getElementById('txti')
     let fim = document.getElementById('txtf')
     let passo = document.getElementById('txtp')
     let res = document.getElementById('res')
-
-    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0) {
-        res.innerHTML = 'impossivel contar!'
-        window.alert('[ERRO] Faltam dados!')
-
+    if (ini.value.length == 0 || fim.value.length == 0 || passo.value.length == 0){
+        res.innerHTML = ' Impossivel contar!'
+        window.alert('[ERRO] faltam dados!')
     } else {
-        res.innerHTML = 'Contando:. . . '
+        res.innerHTML = 'Contando...'
         let i = Number(ini.value)
         let f = Number(fim.value)
         let p = Number(passo.value)
-        if(p <= 0) {
-            window.alert('Passo inválido! Considerando PASSO 1')
-            p = 1
+        if (p <= 0) {
+            window.alert('Passo invalido! considerando PASSO 1')
+           p = 1 
         }
-        if (i < f) {
-            for(let c = i; c <= f; c += p) {
-            res.innerHTML += `${c} \u{1F449}`
-            }
+        if(i < f){
+            // Contagem crescente
+             for(let c = 1; c <= f; c += p) {
+            res.innerHTML += `${c} \u{1F449}`        
+        }
+        
         } else {
-            for(let c = i; c >= f; c -= p) {
+            // contagem regressiva
+            for(let c = i; c >= f; c-= p) {
                 res.innerHTML += `${c} \u{1F449}`
             }
         }
-            res.innerHTML += `\u{1F3C1}`    
+        res.innerHTML += `\u{1F3C1}`
     }
 }
